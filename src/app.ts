47273864deaba465b5460551c6eb2a94a50d79bb
Fernando Lima <eufernandoprogramador@gmail.com>
2023-08-24 00:14:55 -0300

    prisma integration

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
-import fastify from "fastify";
+import { PrismaClient } from "@prisma/client"
+import fastify from "fastify"
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: "Fernando",
+    email: "<fernando@example.com>",
+  },
+})
+
+// docker run --name api-solid-pg -e POSTGRESQL_USERNAME=docker -e POSTGRESQL_PASSWORD=docker -e POSTGRESQL_DATABASE=apisolid -p 5432:5432 bitnami/postgresql
