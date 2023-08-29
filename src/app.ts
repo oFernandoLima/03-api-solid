933604982b8159b99456c80e68d83927e64be750
Fernando Lima <eufernandoprogramador@gmail.com>
2023-08-29 13:28:32 -0300

    application of use cases and design patterns

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,24 @@
-import { PrismaClient } from "@prisma/client"
 import fastify from "fastify"
+import { appRoutes } from "./http/routes"
+import { ZodError } from "zod"
+import { env } from "./env"
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.register(appRoutes)
 
-prisma.user.create({
-  data: {
-    name: "Fernando",
-    email: "<fernando@example.com>",
-  },
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation error.", issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to an external tool loki DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: "Internal server error." })
 })
-
-// docker run --name api-solid-pg -e POSTGRESQL_USERNAME=docker -e POSTGRESQL_PASSWORD=docker -e POSTGRESQL_DATABASE=apisolid -p 5432:5432 bitnami/postgresql
