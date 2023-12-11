605f5514abe079e4b7911ea74a8d7ec6fac8c3d1
Fernando Lima <eufernandoprogramador@gmail.com>
2023-12-11 20:47:09 -0300

    feat: implementing JWT in fastify

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,11 @@
 import { authenticate } from "./controllers/authenticate"
+import { profile } from "./controllers/profile"
 import { register } from "./controllers/register"
 import { FastifyInstance } from "fastify"
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register)
   app.post("/sessions", authenticate)
+
+  app.get("/me", profile)
 }
