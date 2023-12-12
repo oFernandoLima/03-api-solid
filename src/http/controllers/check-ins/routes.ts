af0357c9f10d7173dea5d7fe70e18484c2d3e2b9
Fernando Lima <eufernandoprogramador@gmail.com>
2023-12-12 02:10:46 -0300

    feat: e2e gym route tests

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -12,5 +12,5 @@ export async function checkInsRoutes(app: FastifyInstance) {
   app.get("/check-ins/metrics", metrics)
 
   app.post("/gyms/:gymId/check-ins", create)
-  app.patch("check-ins/:checkInId/validate", validate)
+  app.patch("/check-ins/:checkInId/validate", validate)
 }
